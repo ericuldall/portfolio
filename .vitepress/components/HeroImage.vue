109af2a5a69965f62ea55c9689e928de19ccc285
Eric Uldall <ericuldall@gmail.com>
2023-10-21 15:00:50 -0700

    even better hero image fixes

--- .vitepress/components/HeroImage.vue
+++ .vitepress/components/HeroImage.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-	<div class="flex justify-center items-center mb-6">
-		<img class="shadow-xl relative rounded-full z-10 w-3/4" src="/eu-headshot.png" />
+	<div class="flex justify-center items-center mb-6 h-full">
+		<img class="shadow-xl relative rounded-full z-10 w-1/2 md:w-3/4" src="/eu-headshot.png" />
 	</div>
 </template>
