94dff7366419d4d208464201ea7f98acb5ee1839
Eric Uldall <ericuldall@gmail.com>
2023-10-21 14:58:14 -0700

    fixed hero image

---
+++ .vitepress/components/HeroImage.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+	<div class="flex justify-center items-center mb-6">
+		<img class="shadow-xl relative rounded-full z-10 w-3/4" src="/eu-headshot.png" />
+	</div>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,11 +3,14 @@ import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './style.css'
 
+import HeroImage from '../components/HeroImage.vue';
+
 export default {
   extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'home-hero-image': () => h(HeroImage)
     })
   },
   enhanceApp({ app, router, siteData }) {
